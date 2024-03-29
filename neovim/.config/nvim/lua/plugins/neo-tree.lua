f679bd34456f47535657b6c0049e7cf190671f03
Marc Goujon <marc.goujon@appfolio.com>
2024-03-28 21:26:26 -0700

    neotree

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,9 +8,16 @@ return {
 	},
 	config = function()
     require('neo-tree').setup({
-    close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+        },
+      },
+      close_if_last_window = true,
     })
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
