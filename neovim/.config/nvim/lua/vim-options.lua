066a7af01d8fe171a333a8279f8fdefa1c8c3900
Marc Goujon <marc.goujon@appfolio.com>
2024-04-11 07:49:05 -0700

    fix global var

--- neovim/.config/nvim/lua/vim-options.lua
+++ neovim/.config/nvim/lua/vim-options.lua
@@ -3,8 +3,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set cmdheight=1")
+
+vim.cmd("set clipboard=unnamed")
+
+-- leader key to space --
 vim.g.mapleader = " "
+
 vim.keymap.set('n', '<c-z>', ':undo<CR>')
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
@@ -19,3 +25,5 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
 })
+
+
